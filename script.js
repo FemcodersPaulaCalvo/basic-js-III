6db0f1c936e976fd0a70136b9c95bab29048c1ff
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-04-09 16:54:19 +0200

    feat(script): NUMBERS__Exercise 3

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ function numberRest(num1, num2) {
   return num1 - num2;
 }
 
-console.log(numberRest(10, 7));
+// console.log(numberRest(10, 7));
+
+// EXERCISE 3
+
+function numberMultiplication(num1, num2) {
+  return num1 * num2;
+}
+
+// console.log(numberMultiplication(15, 4));
