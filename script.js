5b140acbd1257a63849da40711fb410fda0557b9
PCalvoGarcia <paulacalvogarcia@gmail.com>
2025-04-09 16:50:12 +0200

    feat(script): NUMBERS__Exercise 2

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function numberSum(num1, num2) {
 }
 
 // console.log(numberSum(3, 7));
+
+// EXERCISE 2
+function numberRest(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(numberRest(10, 7));
